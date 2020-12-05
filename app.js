34072205d5ca36de2be91f5039b335208c62a067
ahmedelahwany <61867082+ahmedelahwany@users.noreply.github.com>
2020-12-06 00:22:08 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
+
+
+// This is the first Block of the code ; it is responsible for connecting to the API  
 class TimeReporter {
 	constructor() {
 		this.jira = null;
 
 	}
+	
+	// We used the library of Jira-connecter to ease the work of dealing with the Jira Api insead of using node-fetch which is a bit complicated
 
 	JiraConnect() {
 
-		// Jira instance
+		// creating a Jira instance
 		var JiraClient = require("jira-connector");
 
 		this.jira = new JiraClient({
-			host: "cors-anywhere.herokuapp.com", // proxy to pypass cors middleware
+			host: "cors-anywhere.herokuapp.com", // proxy to pypass cors middleware becasue it is not possible to connect directly to another webiste that is not from the same origin
 			path_prefix: "https://teamfalcon.atlassian.net/",
 			strictSSL: false,
 			basic_auth: {
@@ -19,6 +24,9 @@ class TimeReporter {
 			}
 		});
 	}
+	// This async function is responsible for populating the drop menus in the UI ( User, Status , Type ,Project)
+	// get requests are sent to the Jira API through the Jira-connector to get the data needed
+	// try and catch blocks are used to catch any errors while retrieving data
 
 	async getDropMenusDATA() {
 
@@ -56,20 +64,21 @@ class TimeReporter {
 		};
 	}
 
-
+// This async function is used to search issue In the Jira website bases on the chosed values  In the UI
+	// the usual node-fetch method is being used here because Jira-connecter was't working when trying to search for Issues
 	async SearchJira(project, status, type, startDate, endDate) {
 
-		let statusQuery = status == 0 ? " " : `status = ${status} AND  `;
+		let statusQuery = status == 0 ? " " : `status = ${status} AND  `; // status =0 or type = 0 means that the user has chosen All option on the drop menus of the UI ; which means the JQL query won't have the status or type fields
 		let typeQuery = type == 0 ? " " : `type = ${type} AND  `;
 
-		let encodeCreds = btoa("hammoudkhalaf7@gmail.com:Ups9dGVIAQnDVs3NTU2S74F9")
+		let encodeCreds = btoa("hammoudkhalaf7@gmail.com:Ups9dGVIAQnDVs3NTU2S74F9")// encoding the email and the api_token to attch them in the GET request
 		const fetch = require('node-fetch');
 		let encodeuri = encodeURI(`https://cors-anywhere.herokuapp.com/https://teamfalcon.atlassian.net/rest/api/2/search?jql= 
                             project = ${project} AND ${typeQuery} ${statusQuery}  created >= ${startDate} AND created <= ${endDate}`)
 
 		console.log(`project = ${project} AND ${typeQuery} ${statusQuery}  created >= ${startDate} AND created <= ${endDate}`)
 		try {
-			var JiraIssuesInfo = await fetch(encodeuri, {
+			var JiraIssuesInfo = await fetch(encodeuri, {  // sending the request with values to get the issues 
 				method: 'GET',
 				headers: {
 					'Authorization': 'Basic ' + encodeCreds
@@ -80,7 +89,7 @@ class TimeReporter {
 			console.log(err);
 		}
 		var JiraIssuesJson = await JiraIssuesInfo.json();
-		var JiraIssues = await JiraIssuesJson.issues;
+		var JiraIssues = await JiraIssuesJson.issues; // getting issues from the retrieved Json Data
 
 		console.log(JiraIssues);
 		return {
@@ -90,11 +99,11 @@ class TimeReporter {
 	}
 }
 
-
+// This The UI class which is resppnsible for any thing that needs to be executed on the UI , it is totally seperated from The Searching in the Jira API
 class UI {
 	constructor() {
-		this.loader = document.querySelector(".loader");
-
+		this.loader = document.querySelector(".loader");// This loader element will be used In UI to be shown when user clicks (create Report) button and will be hidden when the data is retrieved from the Jira API
+              // We used a library to get the dataPicker element in the UI to ease our work insead of hardcoding the datapicker
 		$(function () {
 			$('input[name="daterange"]').daterangepicker({
 				opens: 'left'
@@ -103,16 +112,17 @@ class UI {
 			});
 		});
 	}
-
+       // this function is used to add the data retrieved from the API  to drop menus( HTML select elements) ( Status, Project,Type),
 	addSelecttOptions(options, Component) {
 		const component = document.getElementById(Component);
-		let output = Component == "Project" ? "" : "<option value='0' selected>All</option>";
+		let output = Component == "Project" ? "" : "<option value='0' selected>All</option>";// 
 		options.forEach(option => {
-			output += `<option value ="${option.id}">${option.name}</option>`;
-		});
+			output += `<option value ="${option.id}">${option.name}</option>`;// the value of the option will be the ID , to be able to use later when the user choose an option form the select menu options
+		});// the text content will be the name of the data ( status name, project name or type name) 
 		component.innerHTML = output;
 	}
-     
+            // this function is used to add the data retrieved from the API  to drop menu( HTML select elements) ( User),
+
 	addUserOptions(options) {
 		const user = document.getElementById("User");
 		let output = "<option value='0' selected>none</option> <option value='2' selected>All</option>";
@@ -121,7 +131,8 @@ class UI {
 		});
 		user.innerHTML = output;
 	}
-
+// This function  (we Got it from the Internet) is used to convert seconds to days , hours , minutes and seconds. It is used to convert the estimated , remaining and logged time of the issues
+	//we tried to use some libraries instead of this function, but they weren't work fine 
 	formatDuration(seconds) {
 		var days = Math.floor(seconds / (24 * 60 * 60));
 		seconds -= days * (24 * 60 * 60);
@@ -131,7 +142,7 @@ class UI {
 		seconds -= minutes * (60);
 		return ((0 < days) ? (days + " day, ") : "") + hours + "h, " + minutes + "m and " + seconds + "s"
 	}
-
+// This function is responsible for 
 	showTableData(user, aggregationField, issues) {
 		
 		this.hideLoader();
